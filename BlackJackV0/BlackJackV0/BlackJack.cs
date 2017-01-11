f24948d36c6b004939ffed9cd5b5bcf12fcb03ab
K8760 <K8760@ICT-421-WS-27>
2017-01-11 14:40:20 +0200

    tehty

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -8,26 +8,38 @@ namespace JAMK.IT
   {
     static void Main()
     {
-      int myNumber = 17;
+    while (true)
+    {
+                Random rnd = new Random();
+      int myNumber = rnd.Next(10,21);
       int theirNumber;
       System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
-        {
-          System.Console.WriteLine("You win.");
-        }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
+            //System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+            //reading and converting 
+                System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                string teksti = System.Console.ReadLine();
+                if (teksti == "exit" || teksti == "x")
+                {
+                    break;
+                }
+                theirNumber = System.Convert.ToInt32(teksti);
+                //comparing that given number is valid
+                if (theirNumber < 1 || theirNumber > 21)
+                {
+                    Console.WriteLine("The given number is out of limits, try again.");
+                }
+                else
+                {
+                //comparing
+                if (theirNumber >= myNumber && theirNumber <= 21)
+                {
+                    System.Console.WriteLine("You win.");
+                }
+                else
+                {
+                    System.Console.WriteLine("You lose.");
+                }
+              }
       }
     }
   }
